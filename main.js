1b946c7b84006be4ec0352660dea40302dc410f1
Jânderson Machado <jandersonmachado090@gmail.com>
2023-02-22 22:44:34 -0300

    new file

---
+++ main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const InputNomeContato = document.getElementById('nome-contato');
+    const InputNumeroContato = document.getElementById('numero-contato');
+
+    let linha = '<tr>';
+    linha += `<td>${InputNomeContato.value}</td>`;
+    linha += `<td>${InputNumeroContato.value}</td>`;
+    linha += `<td.${InputNumeroContato.value >= 9999999999 ? 'Concluido' : 'Erro'}</td>`;
+    linha += `<td>${}</td>`;
+    linha += `</tr>`;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHtml = linha;
+});
